297a482bcbaa7b3bf0847df9227366b2c4d99ae4
Tirath Bhathawala <tirath.bhathawala@gmail.com>
2024-07-22 21:37:28 +0530

    Update docker for lstm model instead of prebuilt model

--- docker/app/test_server.py
+++ docker/app/test_server.py
@@ -1,6 +1,6 @@
 import unittest
 import json
-from app.server import app
+from server import app
 
 class FlaskAppTestCase(unittest.TestCase):
     def setUp(self):
@@ -8,14 +8,14 @@ class FlaskAppTestCase(unittest.TestCase):
         self.app.testing = True
 
     def test_predict(self):
-        payload = {"text": "jews are such self-serving people, exactly my kind"}
+        payload = {"text": "jews are such self-serving people"}
         response = self.app.post('/predict',
                                  data=json.dumps(payload),
                                  content_type='application/json')
         self.assertEqual(response.status_code, 200)
-        data = json.loads(response.get_data(as_text=True))
-        self.assertIn('not hate', data['labels'])
-        self.assertGreater(data['scores'][0], 0.5)
+        data = json.loads(response.get_data())
+        self.assertIn('hate', data['prediction'])
+        self.assertGreater(data['probability'][0], 0.5)
 
 if __name__ == '__main__':
     unittest.main()
